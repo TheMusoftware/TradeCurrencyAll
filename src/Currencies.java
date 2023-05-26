0b0f80b962b64316d273bd8801bf2974288e9599
musoftware <mkazi581@gmail.com>
2023-05-26 21:21:00 +0300

    updated by dev

---
+++ src/Currencies.java
@@ -0,0 +1,26 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public abstract class Currencies {
+    private String code;
+    private double totalInWallet;
+    private double currentPrice; // Planning feature
+    void createFiles() throws IOException {
+        String path = "src/Datas/"+code+".txt";
+        File file = new File(path);
+        if(!file.exists()){
+            file.createNewFile();
+            FileWriter writer = new FileWriter(file);
+            PrintWriter printWriter = new PrintWriter(writer);
+            Date date = new Date();
+            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss - dd.MMMM.yyyy");
+            printWriter.println("New currency "+ code+" is created on: "+sdf.format(date));
+            printWriter.close();
+        }
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,24 @@
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Main {
-    public static void main(String[] args) {
+    static String logPath = "src/Datas/logs.txt";
+   static void CreateAndCheckLogFiles() throws IOException {
+        File file = new File(logPath);
+        if(!file.exists()){
+            file.createNewFile();
+            FileWriter writer = new FileWriter(file);
+            PrintWriter printWriter = new PrintWriter(writer);
+            Date date = new Date();
+            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss - dd.MMMM.yyyy");
+            printWriter.println("This file created on : "+sdf.format(date));
+            printWriter.close();
+        }
+
+    }
+    public static void main(String[] args) throws IOException {
+        CreateAndCheckLogFiles();
+
     }
 }
