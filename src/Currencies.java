fa5e1677362a9f8c456cc2dc292398149802b372
musoftware <mkazi581@gmail.com>
2023-05-28 13:18:33 +0300

    updated by dev

--- src/Currencies.java
+++ src/Currencies.java
@@ -4,13 +4,18 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Scanner;
 
 public  class Currencies {
+    Scanner scanner = new Scanner(System.in);
     private String code;
+   private String path = "src/Datas/"+code+".txt";
+    SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss - dd.MMMM.yyyy");
     private double totalInWallet;
+    private double totalDeposit;
     private double currentPrice; // Planning feature
+    private double average;
     void createFiles() throws IOException {
-        String path = "src/Datas/"+code+".txt";
         File file = new File(path);
         if(!file.exists()){
             file.createNewFile();
@@ -26,7 +31,31 @@ public  class Currencies {
     public Currencies(String code) throws IOException {
         this.code = code;
         totalInWallet = 0;
-
+        average = 0;
+        totalDeposit = 0;
         createFiles();
+    }
+    public  void buy() throws IOException {
+        System.out.print("Enter amount: ");
+        double amount = scanner.nextDouble();
+        totalInWallet+=amount;
+        System.out.println();
+        System.out.print("Enter buying price: ");
+        double price = scanner.nextDouble();
+        totalDeposit+=amount*price;
+        File file = new File(path);
+        FileWriter fileWriter = new FileWriter(file,true);
+        PrintWriter printWriter = new PrintWriter(fileWriter);
+        Date date = new Date();
+        printWriter.println(convertLine(amount,price) + "Buying on :"+sdf.format(date));
+        printWriter.close();
+    }
+    public void sell(){
+
+    }
+    private String convertLine(double amount, double price){
+        String str = "";
+        str+="Amount: <#>"+amount+"<#> Price: <#>"+price+"<#>";
+        return str;
     }
 }
