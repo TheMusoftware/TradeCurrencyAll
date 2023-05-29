55384151aadd1c0000f0aa9506b4943f8561ac2a
musoftware <mkazi581@gmail.com>
2023-05-29 15:47:24 +0300

    updated by dev

--- src/Currencies.java
+++ src/Currencies.java
@@ -119,6 +119,14 @@ public  class Currencies {
         return totalInWallet;
     }
 
+    public double getCurrentPrice() {
+        return currentPrice;
+    }
+
+    public void setCurrentPrice(double currentPrice) {
+        this.currentPrice = currentPrice;
+    }
+
     @Override
     public String toString(){
         String str = "---";
